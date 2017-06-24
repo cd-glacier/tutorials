72dde5610dee657840db360c0e566af86a768c41
g-hyoga <hyoga0216@gmail.com>
2017-06-24 17:45:12 +0900

    Q5

--- Fringe81/05_passStudents.scala
+++ Fringe81/05_passStudents.scala
@@ -4,13 +4,16 @@ object Main {
     m.isDefinedAt("math") && m.isDefinedAt("english")
   }
 
-  def passStudents(scores: Map[String, Map[String, Int]]): Map[String, Int] = {
-    val result: Map[String, Map[String, Int]]
-    scores.foreach{ case (k, v) => 
-      if (haveEnglishAndMath(v)) {
-        if ((v("english") + v("math") / 2) >= 80){
-          result += k -> v
-        }
+  def passStudents(scores: Map[String, Map[String, Int]]): collection.mutable.Map[String, Int] = {
+    val result = collection.mutable.Map.empty[String, Int]
+    scores.foreach{ case (name, score) =>
+      val average = if (haveEnglishAndMath(score)) {
+        (score("math")+score("english")) / 2
+      } else {
+        0
+      }
+      if (haveEnglishAndMath(score) && average >= 80) {
+        result += name -> average
       }
     }
     return result
@@ -18,14 +21,14 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val scores = Map(
-     "ichiro" -> Map("math"->82, "english"->99),
-     "jiro" -> Map("japanese"->100, "social"->88),
-     "saburo" -> Map("math"->76, "english"->80),
-     "shiro" -> Map("math"->99, "social"->81),
-     "hanako" -> Map("math"->84, "english"->78, "social"->66))
+      "ichiro" -> Map("math"->82, "english"->99),
+      "jiro" -> Map("japanese"->100, "social"->88),
+      "saburo" -> Map("math"->76, "english"->80),
+      "shiro" -> Map("math"->99, "social"->81),
+      "hanako" -> Map("math"->84, "english"->78, "social"->66))
 
-   println(passStudents(scores))
- }
+    println(passStudents(scores))
+  }
 }
 
 
