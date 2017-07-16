8dae355327b307b586906239dcf52554a09a8ddf
g-hyoga <hyoga0216@gmail.com>
2017-07-16 15:38:34 +0900

    q2

--- Fringe81/src/main/scala/02_getFirst.scala
+++ Fringe81/src/main/scala/02_getFirst.scala
@@ -1,6 +1,7 @@
 object GetFirst {
   def getFirst(value: Option[String]): String = {
-    value.getOrElse("").headOption.getOrElse("").toString
+    // Optionにつつまれている時はgetで取り出してから使う必要があると思い込んでいました...
+    value.headOption.getOrElse("").toString
   }
 
   def main(args: Array[String]): Unit = {
