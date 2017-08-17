78f035a0960f4f5e253d007ae31a410874b7742e
g-hyoga <hyoga0216@gmail.com>
2017-08-17 20:24:23 +0900

    03

--- Fringe81/src/main/scala/03_translate.scala
+++ Fringe81/src/main/scala/03_translate.scala
@@ -1,8 +1,15 @@
 object NumEnglishDictionary {
   private val dictionary = Map(1->"one", 2->"two")
   def translate(num: Option[Int]): Option[String] = {
-    num.flatMap(dictionary.get(_))
+    num.flatMap(dictionary.get)
   }
+  
+  def translate2(num: Option[Int]): Option[String] = {
+    num.collect{ case i if dictionary.isDefinedAt(i) => 
+      dictionary.get(i).get
+    }
+  }
+
 }
 
 object Main {
@@ -10,6 +17,10 @@ object Main {
     println(NumEnglishDictionary.translate(Some(1)))
     println(NumEnglishDictionary.translate(Some(3)))
     println(NumEnglishDictionary.translate(None))
+
+    println(NumEnglishDictionary.translate2(Some(1)))
+    println(NumEnglishDictionary.translate2(Some(3)))
+    println(NumEnglishDictionary.translate2(None))
   }
 }
 
